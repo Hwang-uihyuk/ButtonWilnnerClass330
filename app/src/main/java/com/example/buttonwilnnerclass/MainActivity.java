d7a4c07f4eefaf1d9140573faf05d652fd5cb52f
Hwang-uihyuk <weihyuk39@gmail.com>
2021-03-30 11:44:23 +0900

    one listener for two buttons

--- app/src/main/java/com/example/buttonwilnnerclass/MainActivity.java
+++ app/src/main/java/com/example/buttonwilnnerclass/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     Button mButton1, mButton2;
     TextView mTextView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,22 +21,23 @@ public class MainActivity extends AppCompatActivity {
         mTextView1 = findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(new MyOnClickListener());
-        mButton2.setOnClickListener(new MyOnClickListener2());
+        mButton2.setOnClickListener(new MyOnClickListener());
     }
 
     class MyOnClickListener implements View.OnClickListener {
         @Override
-        public void onClick(View v){
-          mTextView1.setText("You clicked HYUK'S bUTTON");
-
-
-        }
-    }
-
-    private class MyOnClickListener2 implements View.OnClickListener {
-        @Override
         public void onClick(View v) {
-            mTextView1.setText("You clicked my 2nd bUTTON");
+            switch(v.getId()) {
+                case R.id.button1:
+                    mTextView1.setText("You clicked HYUK'S bUTTON");
+                    break;
+                case R.id.button2:
+                    mTextView1.setText("You clicked my 2nd bUTTON");
+                break;
+                default :
+                    break;
         }
     }
+
+}
 }
